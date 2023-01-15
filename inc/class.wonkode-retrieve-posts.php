2e491e53f0bbc6384adc8abcdf4ceeab234f5a11
Wondwossen Birhanie <wonsend35@gmail.com>
2023-01-15 23:01:06 +0300

    base class to retrieve posts data

---
+++ inc/class.wonkode-retrieve-posts.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Class to be used to retrieve post data
+ * 
+ * @package WonKode
+ * @since 1.0
+ */
+// restrict direct access
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+if ( ! class_exists( 'WonKode_Retrieve_Posts' ) ) {
+    class WonKode_Retrieve_Posts {
+
+    } // ENDS --- class
+}
