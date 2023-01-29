368fdaa78e45d275ed8b8ba4c3392e5b798dc9b8
Wondwossen Birhanie <wonsend35@gmail.com>
2023-01-29 23:42:06 +0300

    template parts builder class included and some other modifications on including files

--- functions.php
+++ functions.php
@@ -5,7 +5,10 @@
  * @package WonKode
  * @since 1.0
  */
-
+// restricting direct access
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 /**
  * First, let's set the maximum content width based on the theme's
  * design and stylesheet.
@@ -44,44 +47,49 @@ if ( ! defined( 'WK_ASSETS_URL' ) ) {
 
 
 // theme set up
-require WK_INC_PATH . '/class.wonkode-setup.php';
+require_once WK_INC_PATH . '/class.wonkode-setup.php';
 WonKode_Setup::get_instance();
 
 // enqueues
-require WK_INC_PATH . '/class.wonkode-enqueues.php';
+require_once WK_INC_PATH . '/class.wonkode-enqueues.php';
 WonKode_Enqueues::init();
 
 // includes
-require WK_INC_PATH . '/class.wonkode-helper.php';
-require WK_INC_PATH . '/class.wonkode-svg-resources.php';
-require WK_INC_PATH . '/customizer/customizer-functions.php';
+require_once WK_INC_PATH . '/class.wonkode-helper.php';
+require_once WK_INC_PATH . '/class.wonkode-svg-resources.php';
+require_once WK_INC_PATH . '/customizer/customizer-functions.php';
 // load class that retrieves post content
-require WK_INC_PATH . '/class.wonkode-retrieve-post-content.php';
-// require WK_INC_PATH . '/class.wonkode-custom-nav-walker.php';
-require WK_INC_PATH . '/class.wp-bootstrap-navwalker.php';
+require_once WK_INC_PATH . '/class.wonkode-retrieve-post-content.php';
+// require_once WK_INC_PATH . '/class.wonkode-custom-nav-walker.php';
+require_once WK_INC_PATH . '/class.wp-bootstrap-navwalker.php';
 
 // widget areas and custom widgets
-require WK_INC_PATH . '/widgets.php';
+require_once WK_INC_PATH . '/widgets.php';
 
 // template tags
-require WK_INC_PATH . '/template-tags.php';
+require_once WK_INC_PATH . '/template-tags.php';
 
 
 /**
  * Load template builders
  */
 // UI components builder base class
-require WK_INC_PATH . '/template-builders/ui-classes/class.wonkode-ui-components.php';
+require_once WK_INC_PATH . '/template-builders/ui-classes/class.wonkode-ui-components.php';
 // Comments feature template class
-require WK_INC_PATH . '/template-builders/class.wonkode-comments-feature.php';
+require_once WK_INC_PATH . '/template-builders/class.wonkode-comments-feature.php';
 // Social media links feature template class
-require WK_INC_PATH . '/template-builders/class.wonkode-social-media-links.php';
+require_once WK_INC_PATH . '/template-builders/class.wonkode-social-media-links.php';
 // Social media sharing feature template class
-require WK_INC_PATH . '/template-builders/class.wonkode-social-media-share-menu.php';
+require_once WK_INC_PATH . '/template-builders/class.wonkode-social-media-share-menu.php';
 // Site content area template builder class
-require WK_INC_PATH . '/template-builders/class.wonkode-site-content-area.php';
+require_once WK_INC_PATH . '/template-builders/class.wonkode-site-content-area.php';
 // Cards template class
-require WK_INC_PATH . '/template-builders/ui-classes/class.wonkode-cards.php';
+require_once WK_INC_PATH . '/template-builders/ui-classes/class.wonkode-cards.php';
+
+// class for content template parts
+require_once WK_INC_PATH . '/template-builders/class.wonkode-content-template-parts.php';
+// global variable for rendering template parts
+$wonkode_content_template_parts = WonKode_Content_Template_Parts::init();
 
 /**
  * --------
@@ -91,7 +99,7 @@ require WK_INC_PATH . '/template-builders/ui-classes/class.wonkode-cards.php';
  */
 
 // hooks
-require WK_INC_PATH . '/hooks.php';
+require_once WK_INC_PATH . '/hooks.php';
 init_wonkode_comment_feature_hooks();
 init_wonkode_social_share_hooks();
 
