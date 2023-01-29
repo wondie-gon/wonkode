c3e40716056270dfcc6eab5ab80f210696d9eeb8
Wondwossen Birhanie <wonsend35@gmail.com>
2023-01-29 23:38:04 +0300

    modified archive template file

--- archive.php
+++ archive.php
@@ -17,19 +17,19 @@ WonKode_Site_Content_Area::open_outer_container( 'bg-light' );
         get_template_part( 'template-parts/post/archive-header' );
         // check post loop
         if ( have_posts() ) {
-        // open wrapping row
-        WonKode_Site_Content_Area::open_content_wrapper_row( 'archive-posts-wrapper py-5' );
-            // open post content column
-            WonKode_Site_Content_Area::open_main_post_col( 'posts-list-col' );
-                while ( have_posts() ) {
-                    the_post();
-                    // get template part
-                    get_template_part( 'template-parts/content/content', 'archive' );
-                }
-            // close post content column
-            WonKode_Site_Content_Area::close_main_post_col();
-            // render sidebar
-            get_sidebar();
+            // open wrapping row
+            WonKode_Site_Content_Area::open_content_wrapper_row( 'archive-posts-wrapper py-5' );
+                // open post content column
+                WonKode_Site_Content_Area::open_main_post_col( 'posts-list-col' );
+                    while ( have_posts() ) {
+                        the_post();
+                        // get template part
+                        get_template_part( 'template-parts/content/content', 'archive' );
+                    }
+                // close post content column
+                WonKode_Site_Content_Area::close_main_post_col();
+                // render sidebar
+                get_sidebar();
             // close wrapping row
             WonKode_Site_Content_Area::close_content_wrapper_row();
         
