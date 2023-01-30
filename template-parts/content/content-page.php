1832ae845368f40494b0c013464e231cb2fea6cc
Wondwossen Birhanie <wonsend35@gmail.com>
2023-01-30 17:45:53 +0300

    changes on class of wrapper

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $wonkode_content_template_parts;
 
 // classes to add to page content
-$wonkode_page_cotent_wrapper_cls = 'col-12 mb-4 page-content';
+$wonkode_page_cotent_wrapper_cls = 'col-12 page-content';
 
 // display page content template
 $wonkode_content_template_parts::page_content( $wonkode_page_cotent_wrapper_cls );
