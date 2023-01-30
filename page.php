fa77b23e4368a24ca7ec759bae229f21395ef6d8
Wondwossen Birhanie <wonsend35@gmail.com>
2023-01-30 17:42:55 +0300

    display comments template rather than in content template parts if it is open for page

--- page.php
+++ page.php
@@ -13,12 +13,19 @@ WonKode_Site_Content_Area::open_outer_container( 'bg-light' );
         // get page header
         get_template_part( 'template-parts/page/page-header' );
         // open wrapping row
-        WonKode_Site_Content_Area::open_content_wrapper_row( 'page-posts-wrapper py-5' );
+        WonKode_Site_Content_Area::open_content_wrapper_row( 'page-posts-wrapper py-5 mb-4' );
             // page content loop
             while ( have_posts() ) {
                 the_post();
                 // get page content template
                 get_template_part( 'template-parts/content/content', 'page' );
+            }
+            /**
+            * Displays comments area with comment form if comments are open, 
+            * or there is some number of comments, and password is not required
+            */
+            if ( ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
+                WonKode_Comments_Feature::show_comments_block( 'col-12 p-4' );
             }
         // close wrapping row
         WonKode_Site_Content_Area::close_content_wrapper_row();
