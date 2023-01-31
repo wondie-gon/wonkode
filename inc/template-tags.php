54ee22ed3f72c03601531b9e5b523922df1bb06f
Wondwossen Birhanie <wonsend35@gmail.com>
2023-01-31 16:51:31 +0300

    Added custom minimal posted on and posted by metas in addition to template tag that displays both at same time, and better commenting of template tags

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * Custom template tags for this theme
+ * Custom template tags for theme.
  *
  * Eventually, some of the functionality here could be replaced by core features.
- *
+ * 
  * @package WonKode
+ * @since 1.0
  */
 
 // Exit if accessed directly.
@@ -13,11 +14,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Prints HTML with meta information for the current post-date/time and author.
- * 
- * @since 1.0
+ * Prints HTML with meta information for 
+ * the current post-date/time and author.
  */
 if ( ! function_exists( 'wonkode_posted_on_by_meta' ) ) {
+	/**
+	 * Prints HTML with meta information for both 
+	 * the current post-date/time and author in one line.
+	 * 
+	 * @since 1.0
+	 * @return void
+	 */
 	function wonkode_posted_on_by_meta() {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
@@ -53,14 +60,68 @@ if ( ! function_exists( 'wonkode_posted_on_by_meta' ) ) {
 		echo '<ul class="nav">' . $posted_on . $byline . '</ul>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
+/**
+ * Minimal posted on time.
+ */
+if ( ! function_exists( 'wonkode_minimal_posted_on' ) ) {
+	/**
+	 * Renders a minimal posted on date meta 
+	 * without date of updates.
+	 * 
+	 * @since 1.0
+	 * @return void
+	 */
+	function wonkode_minimal_posted_on() {
+		$posted_on_time = sprintf(
+			'<time class="entry-date published updated small fst-italic" datetime="%1$s">%2$s</time>',
+			esc_attr( get_the_date( 'c' ) ),
+			esc_html( get_the_date() )
+		);
+
+		echo '<span class="posted-on">';
+		printf( 
+			/* translators: %s: Post published date */
+			esc_html__( 'Posted: %s', 'wonkode' ), 
+			$posted_on_time // phpcs:ignore WordPress.Security.EscapeOutput
+		);
+		echo '</span>';
+	}
+}
+
+if ( ! function_exists( 'wonkode_minimal_posted_by' ) ) {
+	/**
+	 * Renders a minimal posted by author meta 
+	 * only.
+	 * 
+	 * @since 1.0
+	 * @return void
+	 */
+	function wonkode_minimal_posted_by() {
+		if ( ! get_the_author_meta( 'description' ) && post_type_supports( get_post_type(), 'author' ) ) {
+			echo '<span class="posted-by byline">';
+			printf(
+				/* translators: %s: Author name. */
+				esc_html__( 'By %s', 'wonkode' ),
+				'<a href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" class="small fst-italic" rel="author">' . esc_html( get_the_author() ) . '</a>'
+			);
+			echo '</span>';
+		}
+	}
+}
 
 /**
- * Prints background image style for page headers
- * 
- * @since 1.0
- * @param string  $asset_img_file Optional. Image file name in assets folder of theme.
+ * Prints background image style 
+ * for page headers.
  */
 if ( ! function_exists( 'wonkode_page_header_bg_img_style' ) ) {
+	/**
+	 * Prints background image style for page headers
+	 * 
+	 * @since 1.0
+	 * @param string  $asset_img_file Optional. Image file name in 
+	 * 											assets folder of theme.
+	 * @return void
+	 */
 	function wonkode_page_header_bg_img_style( $asset_img_file = '' ) {
 		$hdr_bg_style = wonkode_get_page_header_bg_img_style( $asset_img_file );
 		echo $hdr_bg_style;
@@ -68,15 +129,19 @@ if ( ! function_exists( 'wonkode_page_header_bg_img_style' ) ) {
 }
 
 /**
- * Returns background image style for page headers
- * 
- * @since 1.0
- * @param string  $asset_img_file	Optional. Image file name 
- * 								in assets folder of theme.
- * @return string 	A style attribute to be used inside header 
- * 					html tags to set its background image
+ * Function to get background image style 
+ * for page headers
  */
 if ( ! function_exists( 'wonkode_get_page_header_bg_img_style' ) ) {
+	/**
+	 * Returns background image style for page headers
+	 * 
+	 * @since 1.0
+	 * @param string  $asset_img_file	Optional. Image file name 
+	 * 								in assets folder of theme.
+	 * @return string 	A style attribute to be used inside header 
+	 * 					html tags to set its background image
+	 */
 	function wonkode_get_page_header_bg_img_style( $asset_img_file = '' ) {
 		if ( empty( $asset_img_file ) ) {
 			// get image url for header
@@ -102,15 +167,19 @@ if ( ! function_exists( 'wonkode_get_page_header_bg_img_style' ) ) {
 }
 
 /**
- * Returns post thumbnail url or asset image url, or 
- * the set header image url of the theme for background 
- * image of page header
- * 
- * @since 1.0
- * @param string  $asset_img_file Optional. Image file name in assets folder of theme.
- * @return string Url of image that can be used for background image value
+ * Function to get post thumbnail url or asset image url, or 
+ * the set header image url.
  */
 if ( ! function_exists( 'wonkode_get_page_header_bg_img_url' ) ) {
+	/**
+	 * Returns post thumbnail url or asset image url, or 
+	 * the set header image url of the theme for background 
+	 * image of page header
+	 * 
+	 * @since 1.0
+	 * @param string  $asset_img_file Optional. Image file name in assets folder of theme.
+	 * @return string Url of image that can be used for background image value
+	 */
 	function wonkode_get_page_header_bg_img_url( $asset_img_file = '' ) {
 		$img_url = '';
 		if ( ( is_page() || is_single() ) && has_post_thumbnail() ) {
@@ -136,17 +205,20 @@ if ( ! function_exists( 'wonkode_get_page_header_bg_img_url' ) ) {
 	}
 }
 /**
- * Returns url of image in assets folder or false if passed image name
- * does not fulfill requirements
- * 
- * @since 1.0
- * @param string  		$asset_img_file Optional. Image file name in assets folder of theme.
- * @param array  		$allowed_types Optional. Array of allowed image file types/extensions
- * @return string/bool 	Url of image in theme folder '/assets/images/' or false 
- * 						if none is found or empty string is passed for $asset_img_file, 
- * 						or image extension is not in $allowed_types types
+ * Function to get url of image in assets folder.
  */
 if ( ! function_exists( 'wonkode_get_theme_asset_img_url' ) ) {
+	/**
+	 * Returns url of image in assets folder or false if passed image name
+	 * does not fulfill requirements
+	 * 
+	 * @since 1.0
+	 * @param string  		$asset_img_file Optional. Image file name in assets folder of theme.
+	 * @param array  		$allowed_types Optional. Array of allowed image file types/extensions
+	 * @return string/bool 	Url of image in theme folder '/assets/images/' or false 
+	 * 						if none is found or empty string is passed for $asset_img_file, 
+	 * 						or image extension is not in $allowed_types types
+	 */
 	function wonkode_get_theme_asset_img_url( $asset_img_file, $allowed_types = array() ) {
 		if ( empty( $asset_img_file ) ) {
 			return false;
@@ -173,56 +245,63 @@ if ( ! function_exists( 'wonkode_get_theme_asset_img_url' ) ) {
 }
 
 /**
- * Returns svg resources in group of symbols that can be
- * printed just below opening of <body> html tag for the page 
- * where the resources are needed.
- * 
- * @since 1.0
- * @param string $group 	Name of svg resource group you want 
- * 							to retrieve
- * @param array $names 		Array of svg resource names from group. 
- * 							See available SVGs in 'WonKode_SVG_Resources' 
- * 							class.
- * @param array $args 		{
- * 		Array of arguments you want to modify in the <symbol> tag
- * 		while retrieving svg resources. Default is empty.
- * 			Default values are for icon svg resources
- * 			@type string 	'view_box'		Value for 'viewBox' attribute. Default "0 0 24 24"
- * 			@type int 		'size'			Value for 'size' attribute. Default 24
- * 			@type int 		'stroke_w'		Value for 'stroke-width' attribute. Default 2
- * 			@type string 	'stroke'		Value for 'stroke' (color) attribute. Default 'currentColor'
- * 			@type string 	'fill'			Value for 'fill' (color) attribute. Default 'none'
- * }
- * @return mixed Bundle of SVG symbols
+ * Function to get svg resources in group of symbols.
  */
 if ( ! function_exists( 'wonkode_get_svg_icon_symbols_bundle' ) ) {
+	/**
+	 * Returns svg resources in group of symbols that can be
+	 * printed just below opening of <body> html tag for the page 
+	 * where the resources are needed.
+	 * 
+	 * @since 1.0
+	 * @param string $group 	Name of svg resource group you want 
+	 * 							to retrieve
+	 * @param array $names 		Array of svg resource names from group. 
+	 * 							See available SVGs in 'WonKode_SVG_Resources' 
+	 * 							class.
+	 * @param array $args 		{
+	 * 		Array of arguments you want to modify in the <symbol> tag
+	 * 		while retrieving svg resources. Default is empty.
+	 * 			Default values are for icon svg resources
+	 * 			@type string 	'view_box'		Value for 'viewBox' attribute. Default "0 0 24 24"
+	 * 			@type int 		'size'			Value for 'size' attribute. Default 24
+	 * 			@type int 		'stroke_w'		Value for 'stroke-width' attribute. Default 2
+	 * 			@type string 	'stroke'		Value for 'stroke' (color) attribute. Default 'currentColor'
+	 * 			@type string 	'fill'			Value for 'fill' (color) attribute. Default 'none'
+	 * }
+	 * @return mixed Bundle of SVG symbols
+	 */
 	function wonkode_get_svg_icon_symbols_bundle( $group, $names = array(), $args = array() ) {
 		return WonKode_SVG_Resources::get_icon_symbols_group( $group, $names, $args );
 	}
 }
 
 /**
- * Returns list of svg symbol resources for illustration images
- * 
- * @since 1.0
- * @param array $names      Array of svg icon names stored in 
- *                          self::$illustration_imgs
- * @param array $args 		{
- *  		Array of arguments you want to modify in the <symbol> tag
- *          while retrieving svg resources. Default is empty. 
- *            @type string 'view_box'		Value for 'viewBox' attribute. Default "0 0 1000 1000"
- *            @type string 'x'		        Value for 'x' attribute. Default '0px'
- *            @type string 'y'		        Value for 'y' attribute. Default '0px'
- *            @type string 'bg_shape'		Name of background shape. 
- *                                          Default 'rectangle'. Possible values include 'circle, ellipse'
- *            @type string 'bg_stroke'		Value for 'stroke' (color) attribute of background shape. 
- *                                          Default 'none'
- *            @type string 'bg_fill'		Value for 'fill' (color) attribute of background shape. 
- *                                          Default '#EAECEF'
- *          }
- * @return mixed    List of svg symbols for illustration images
+ * Function to get list of svg symbol resources 
+ * for illustration images
  */
 if ( ! function_exists( 'wonkode_get_illustration_svgs' ) ) {
+	/**
+	 * Returns list of svg symbol resources for illustration images
+	 * 
+	 * @since 1.0
+	 * @param array $names      Array of svg icon names stored in 
+	 *                          self::$illustration_imgs
+	 * @param array $args 		{
+	 *  		Array of arguments you want to modify in the <symbol> tag
+	 *          while retrieving svg resources. Default is empty. 
+	 *            @type string 'view_box'		Value for 'viewBox' attribute. Default "0 0 1000 1000"
+	 *            @type string 'x'		        Value for 'x' attribute. Default '0px'
+	 *            @type string 'y'		        Value for 'y' attribute. Default '0px'
+	 *            @type string 'bg_shape'		Name of background shape. 
+	 *                                          Default 'rectangle'. Possible values include 'circle, ellipse'
+	 *            @type string 'bg_stroke'		Value for 'stroke' (color) attribute of background shape. 
+	 *                                          Default 'none'
+	 *            @type string 'bg_fill'		Value for 'fill' (color) attribute of background shape. 
+	 *                                          Default '#EAECEF'
+	 *          }
+	 * @return mixed    List of svg symbols for illustration images
+	 */
 	function wonkode_get_illustration_svgs( $import = false, $names = array(), $args = array() ) {
 		$svg_resources = new WonKode_SVG_Resources( $import );
 		return $svg_resources::get_illust_symbols_group( $names, $args );
@@ -230,48 +309,55 @@ if ( ! function_exists( 'wonkode_get_illustration_svgs' ) ) {
 }
 
 /**
- * Returns svg use block for passed name of svg symbol
- * 
- * @since 1.0
- * @param string $name      Name of svg to display from loaded svg symbols. 
- *                          See the available names to use in 
- *                          'WonKode_SVG_Reources' class.
- * @param string $svg_type  Type of svg. Possible values: 'icon' and 'illustration'
- *                          Default 'icon'
- * @param array $args       {
- *      Array of additional arguments to be used as attributes for svg use
- *      Default: empty array
- *          @type string    'class'    Class attribute value
- *          @type int 		'width'    Width for svg to display
- *          @type int 		'height'   Height for svg to display
- * }
- * @return mixed    SVG use block with id attribute value to display
+ * Template function to get svg use block 
+ * for passed name of svg symbol
  */
 if ( ! function_exists( 'wonkode_get_svg_use_block' ) ) {
+	/**
+	 * Returns svg use block for passed name of svg symbol
+	 * 
+	 * @since 1.0
+	 * @param string $name      Name of svg to display from loaded svg symbols. 
+	 *                          See the available names to use in 
+	 *                          'WonKode_SVG_Reources' class.
+	 * @param string $svg_type  Type of svg. Possible values: 'icon' and 'illustration'
+	 *                          Default 'icon'
+	 * @param array $args       {
+	 *      Array of additional arguments to be used as attributes for svg use
+	 *      Default: empty array
+	 *          @type string    'class'    Class attribute value
+	 *          @type int 		'width'    Width for svg to display
+	 *          @type int 		'height'   Height for svg to display
+	 * }
+	 * @return mixed    SVG use block with id attribute value to display
+	 */
 	function wonkode_get_svg_use_block( $name, $svg_type = 'icon', $args = array() ) {
 		return WonKode_SVG_Resources::get_svg_use_block( $name, $svg_type, $args );
 	}
 }
 
 /**
- * Returns svg use block for passed name of svg icon
- * 
- * @since 1.0
- * @param string $name      Name of svg icon to display from loaded svg symbols. 
- *                          See the available names to use in 
- *                          'WonKode_SVG_Reources::$ui_icons' and 
- * 							'WonKode_SVG_Reources::$social_icons'.
- * 
- * @param array $args       {
- *      Array of additional arguments to be used as attributes for svg use
- *      Default: empty array
- *          @type string    'class'    Class attribute value. Default 'svg-icon'
- *          @type int 		'width'    Width for svg to display. Default 24
- *          @type int 		'height'   Height for svg to display. Default 24
- * }
- * @return mixed    SVG use block to display for icon.
+ * Template function to get svg use block for icon SVGs.
  */
 if ( ! function_exists( 'wonkode_get_svg_icon_use' ) ) {
+	/**
+	 * Returns svg use block for passed name of svg icon
+	 * 
+	 * @since 1.0
+	 * @param string $name      Name of svg icon to display from loaded svg symbols. 
+	 *                          See the available names to use in 
+	 *                          'WonKode_SVG_Reources::$ui_icons' and 
+	 * 							'WonKode_SVG_Reources::$social_icons'.
+	 * 
+	 * @param array $args       {
+	 *      Array of additional arguments to be used as attributes for svg use
+	 *      Default: empty array
+	 *          @type string    'class'    Class attribute value. Default 'svg-icon'
+	 *          @type int 		'width'    Width for svg to display. Default 24
+	 *          @type int 		'height'   Height for svg to display. Default 24
+	 * }
+	 * @return mixed    SVG use block to display for icon.
+	 */
 	function wonkode_get_svg_icon_use( $name, $args = array() ) {
 		$svg_type = 'icon';
 		return WonKode_SVG_Resources::get_svg_use_block( $name, $svg_type, $args );
@@ -279,23 +365,27 @@ if ( ! function_exists( 'wonkode_get_svg_icon_use' ) ) {
 }
 
 /**
- * Returns illustration image's svg use block for passed name
- * 
- * @since 1.0
- * @param string $name      Name of svg illustration to display from loaded svg symbols. 
- *                          See the available names to use in 
- *                          'WonKode_SVG_Reources::$illustration_imgs'.
- * 
- * @param array $args       {
- *      Array of additional arguments to be used as attributes for svg use
- *      Default: empty array
- *          @type string    'class'    Class attribute value. Default 'svg-illustration'
- *          @type int 		'width'    Width for svg to display. Default $content_width
- *          @type int 		'height'   Height for svg to display. Default $content_width
- * }
- * @return mixed    SVG use block to display for illustration image.
+ * Template function to get 
+ * illustration image's svg use block.
  */
 if ( ! function_exists( 'wonkode_get_svg_illustration_use' ) ) {
+	/**
+	 * Returns illustration image's svg use block for passed name
+	 * 
+	 * @since 1.0
+	 * @param string $name      Name of svg illustration to display from loaded svg symbols. 
+	 *                          See the available names to use in 
+	 *                          'WonKode_SVG_Reources::$illustration_imgs'.
+	 * 
+	 * @param array $args       {
+	 *      Array of additional arguments to be used as attributes for svg use
+	 *      Default: empty array
+	 *          @type string    'class'    Class attribute value. Default 'svg-illustration'
+	 *          @type int 		'width'    Width for svg to display. Default $content_width
+	 *          @type int 		'height'   Height for svg to display. Default $content_width
+	 * }
+	 * @return mixed    SVG use block to display for illustration image.
+	 */
 	function wonkode_get_svg_illustration_use( $name, $args = array() ) {
 		$svg_type = 'illustration';
 		// modifying width and height
@@ -310,12 +400,16 @@ if ( ! function_exists( 'wonkode_get_svg_illustration_use' ) ) {
 }
 
 /**
- * Returns copyright info of site
- * 
- * @since 1.0
- * @return mixed Html string with copyright info
+ * Template function to get 
+ * copyright info of site.
  */
 if ( ! function_exists( 'wonkode_get_copyright_info' ) ) {
+	/**
+	 * Returns copyright info of site
+	 * 
+	 * @since 1.0
+	 * @return mixed Html string with copyright info
+	 */
 	function wonkode_get_copyright_info() {
 		return apply_filters( 
 			'wonkode_copyright_info', 
@@ -331,11 +425,19 @@ if ( ! function_exists( 'wonkode_get_copyright_info' ) ) {
 }
 
 /**
- * Returns info about theme
- * 
- * @since 1.0
+ * Template function to get 
+ * info about theme to display 
+ * on bottom footer of sites.
  */
 if ( ! function_exists( 'wonkode_get_about_theme' ) ) {
+	/**
+	 * Returns information about theme, 
+	 * powered by info, developers, 
+	 * and related links.
+	 * 
+	 * @since 1.0
+	 * @return mixed Information about theme.
+	 */
 	function wonkode_get_about_theme() {
 		$wonkode_theme = wp_get_theme();
 		return apply_filters( 
@@ -356,13 +458,18 @@ if ( ! function_exists( 'wonkode_get_about_theme' ) ) {
 }
 
 /**
- * Gets current theme name 
- * regardless of the child theme
- * 
- * @return string/bool Name of theme if 
- * it's parent theme, otherwise false
+ * Template function to get 
+ * current theme name.
  */
 if ( ! function_exists( 'wonkode_get_theme_name' ) ) {
+	/**
+	 * Returns current theme name 
+	 * regardless of the child theme
+	 * 
+	 * @since 1.0
+	 * @return string/bool Name of theme if 
+	 * it's parent theme, otherwise false
+	 */
 	function wonkode_get_theme_name() {
 		$current_theme = wp_get_theme();
 
