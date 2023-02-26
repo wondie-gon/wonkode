eda86a595511062a5b0797230695d49022174bb9
Wondwossen Birhanie <wonsend35@gmail.com>
2023-02-26 15:09:25 +0300

    class attributes of page header is modified

--- template-parts/page/page-header.php
+++ template-parts/page/page-header.php
@@ -6,9 +6,9 @@
  * @package WonKode
  */
 ?>
-<div class="row g-5 align-center page-header">
+<div class="row g-5 align-center">
     <?php $page_hdr_bg_img = 'wonkode-bg-img-light.jpg'; ?>
-    <div class="col-12 py-5 animated fadeIn text-center"<?php wonkode_page_header_bg_img_style( $page_hdr_bg_img ); ?>>
+    <div class="col-12 py-5 animated fadeIn page-header"<?php wonkode_page_header_bg_img_style( $page_hdr_bg_img ); ?>>
         <?php single_post_title( '<h1 class="page-title">', '</h1>' ); ?>
-    </div>
-</div><!-- .page-header -->
+    </div><!-- .page-header -->
+</div>
