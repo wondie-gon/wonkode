a370649daad3af89b84e00cff6d8d853efc6b027
Wondwossen Birhanie <wonsend35@gmail.com>
2023-02-26 15:12:10 +0300

    modified class attributes for single and archive template files

--- template-parts/post/archive-header.php
+++ template-parts/post/archive-header.php
@@ -6,10 +6,10 @@
  * @package WonKode
  */
 ?>
-<div class="row g-5 align-center archive-header">
-    <div class="col-12 py-5 animated fadeIn text-center"<?php wonkode_page_header_bg_img_style(); ?>>
+<div class="row g-5 align-center">
+    <div class="col-12 py-5 animated fadeIn archive-header"<?php wonkode_page_header_bg_img_style(); ?>>
         <h1 class="archive-title py-5">
             <?php echo get_the_archive_title(); ?>
         </h1>
-    </div>
-</div><!-- .archive-header -->
+    </div><!-- .archive-header -->
+</div>
--- template-parts/post/single-header.php
+++ template-parts/post/single-header.php
@@ -6,10 +6,10 @@
  * @package WonKode
  */
 ?>
-<div class="row g-5 align-center single-header">
-    <div class="col-12 py-5 animated fadeIn text-center"<?php wonkode_page_header_bg_img_style(); ?>>
+<div class="row g-5 align-center">
+    <div class="col-12 py-5 animated fadeIn single-header"<?php wonkode_page_header_bg_img_style(); ?>>
         <h1 class="single-title py-5">
             <?php single_post_title(); ?>
         </h1>
-    </div>
-</div><!-- .single-header -->
+    </div><!-- .single-header -->
+</div>
