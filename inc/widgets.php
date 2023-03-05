9cc2d6e4b80db4924bff62757f2296948576b985
Wondwossen Birhanie <wonsend35@gmail.com>
2023-03-05 20:03:39 +0300

    New created custom widgets included and initialized

--- inc/widgets.php
+++ inc/widgets.php
@@ -13,6 +13,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 require WK_INC_PATH . '/widgets/class.wonkode-widget-areas.php';
 
 // 'WonKode_Equal_Columns_Image_And_Text' widget
-require WK_INC_PATH . '/widgets/cass.wonkode-equal-columns-image-and-text.php';
+require WK_INC_PATH . '/widgets/class.wonkode-equal-columns-image-and-text.php';
 // 'Leaflet_Custom_Map_Widget'
 require WK_INC_PATH . '/widgets/class.leaflet-custom-map-widget.php';
+// 'WonKode_Short_Footer_About_Site_Text' widget
+require WK_INC_PATH . '/widgets/class.wonkode-short-footer-about-site-text.php';
+// 'WonKode_Footer_Page_Links' widget
+require WK_INC_PATH . '/widgets/class.wonkode-footer-page-links.php';
