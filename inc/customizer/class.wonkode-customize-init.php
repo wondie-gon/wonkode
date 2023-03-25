407e3239e7d530d0de2426160eccaec93835693a
Wondwossen Birhanie <wonsend35@gmail.com>
2023-03-25 16:54:03 +0300

    modified to enqueue customizer control and preview js files for all customizer features

--- inc/customizer/class.wonkode-customize-init.php
+++ inc/customizer/class.wonkode-customize-init.php
@@ -15,16 +15,49 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! class_exists( 'WonKode_Customize_Init' ) ) {
     class WonKode_Customize_Init {
         /**
+         * Theme identifier, namespace
+         * 
+         * @since 1.0
+         * @access protected
+         * @var string 
+         */
+        protected $theme_id;
+        /**
+         * Unique id for prefix
+         * 
+         * @access protected
+         * 
+         * @since 1.0
+         * @var string
+         */
+        protected $prefix_id;
+        /**
          * Class constructor
          */
         public function __construct() {
-            // enqueue styles for custom controls
-            add_action( 'customize_controls_print_styles', array( $this, 'custom_controls_generic_css' ) );
+            // setting theme identity
+            $theme_obj = wp_get_theme();
+            if ( $theme_obj->exists() ) {
+                $this->theme_id = $theme_obj->get( 'TextDomain' );
+            } else {
+                $this->theme_id = get_stylesheet();
+            }
+            // setting unique prefix
+            $this->prefix_id = strtolower( str_replace( '-', '_', $this->theme_id ) );
 
             // load custom control classes
             $this->load_custom_controls();
             // load customizers
             $this->load_customizers();
+
+            // Enqueue customize preview javascript
+            add_action( 'customize_preview_init', array( $this, 'customize_previews_js' ) );
+
+            // enqueue customize controls js
+            add_action( 'customize_controls_enqueue_scripts', array( $this, 'customize_controls_js' ) );
+
+            // enqueue styles for custom controls
+            add_action( 'customize_controls_print_styles', array( $this, 'custom_controls_generic_css' ) );
         }
 
         /**
@@ -111,6 +144,52 @@ if ( ! class_exists( 'WonKode_Customize_Init' ) ) {
 			</style>
 			<?php
 		}
+
+        /**
+		 * Enqueues custom javascript for 
+         * asynchronous real time preview 
+         * of customizer changes.
+         * 
+		 * @since  1.0
+		 */
+		public function customize_previews_js() {
+            // get version
+            $ver = $this->get_scripts_version();
+            // customize assets url
+            $customize_assets_url = get_template_directory_uri() . '/inc/customizer/assets';
+            wp_enqueue_script( $this->theme_id . '-customize-previews', $customize_assets_url . '/js/customize-previews.js', array( 'customize-preview' ), $ver, true );
+        }
+
+        /**
+		 * Enqueues customize controls 
+         * custom javascript
+         * 
+		 * @since  1.0
+		 */
+		public function customize_controls_js() {
+            // get version
+            $ver = $this->get_scripts_version();
+            // customize assets url
+            $customize_assets_url = get_template_directory_uri() . '/inc/customizer/assets';
+            wp_enqueue_script( $this->theme_id . '-customize-controls', $customize_assets_url . '/js/customize-controls.js', array( 'customize-controls', 'jquery' ), $ver, true );
+        }
+
+        /**
+         * Check if theme is in development mode
+         * 
+         * @since 1.0
+         * @return string Version for enqueued scripts
+         */
+        public function get_scripts_version() {
+            $ver = wp_get_theme()->get( 'Version' );
+            // get sitename, and check if it is on local
+		    $sitename = wp_parse_url( network_home_url(), PHP_URL_HOST );
+            // use time as version on local server
+            if ( in_array( strtolower( $sitename ), array( 'localhost', '127.0.0.1' ) ) ) {
+                $ver = time();
+            }
+            return $ver;
+        }
         
     } // ENDS -- class
 }
